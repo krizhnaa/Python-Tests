c5ea2b1efbe1dd0cf2f9aa9c751f1daef552a781
krizhnaa <krizhnaprem@gmail.com>
2023-11-04 20:08:47 +0530

    turtle class ( OOP )

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Just a Repo for all the Objective practises for Python!!!
+
+> 4/11/23 : Object Oriented Programming
--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print('hello')
+from turtle import Turtle, Screen
+
+ramu = Turtle()
+ramu.shape('turtle')
+ramu.color('red','green')
+ramu.setpos(-200,0)
+
+myscreen = Screen()
+
+myscreen.exitonclick()
