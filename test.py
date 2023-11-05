17385acefaa6fc41ca9d26492d84d98262c82e9e
krizhnaa <krizhnaprem@gmail.com>
2023-11-05 19:46:05 +0530

    updated old tutorials

--- test.py
+++ test.py
@@ -1,16 +1,27 @@
-from prettytable import PrettyTable
+# from turtle import Turtle, Screen
+#
+# ramu = Turtle()
+# ramu.shape('turtle')
+# ramu.color('red','green')
+# ramu.setpos(-200,0)
+#
+# myscreen = Screen()
+#
+# myscreen.exitonclick()
 
-pikachu_table = PrettyTable()
-
-pikachu_table.field_names = ['PokeNames','Types']
-pikachu_table.add_rows(
-    [
-        ['Pikachu','Electric'],
-        ['Squirtle','Water'],
-        ['Charmander','Fire']
-    ]
-)
-
-pikachu_table.align = 'l'
-
-print(pikachu_table)
+# from prettytable import PrettyTable
+#
+# pikachu_table = PrettyTable()
+#
+# pikachu_table.field_names = ['PokeNames','Types']
+# pikachu_table.add_rows(
+#     [
+#         ['Pikachu','Electric'],
+#         ['Squirtle','Water'],
+#         ['Charmander','Fire']
+#     ]
+# )
+#
+# pikachu_table.align = 'l'
+#
+# print(pikachu_table)
