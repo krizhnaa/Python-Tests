f1333b73d0600f4652751f24b982068cc8608f4e
krizhnaa <krizhnaprem@gmail.com>
2023-11-05 20:35:29 +0530

    Follower count change

--- test.py
+++ test.py
@@ -30,12 +30,23 @@ class User:
     def __init__(self, id, name):
         self.id = id
         self.name = name
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers+=1
+        self.following+=1
 
 
 user_1 = User("001", "Krishna")
 user_2 = User('002','Vichu')
-print(user_1.id, user_2.name)
+# print(user_1.id, user_2.name)
 
+user_1.follow(user_2)
+print(user_1.name,user_1.followers)
+print(user_1.following)
+print(user_2.name, user_2.followers)
+print(user_2.following)
 
 
 
