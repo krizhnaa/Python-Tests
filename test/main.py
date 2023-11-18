6e240d03603f005a16d0ae7e5826d3e5d9ce1786
krizhnaa <krizhnaprem@gmail.com>
2023-11-18 21:27:19 +0530

    The first name from the list is fetched and stored in the sample letter

--- test/main.py
+++ test/main.py
@@ -4,3 +4,11 @@ names = []
 with open("./Input/Names/invited_names.txt") as file:
     names_file = file.read()
     names = names_file.split('\n')
+
+# data_into_list = data.replace('\n', ' ')
+
+with open("./Input/Letters/starting_letter.txt") as file:
+    letter = file.read()
+    replaced_letter = letter.replace('[name]', names[0])
+
+
