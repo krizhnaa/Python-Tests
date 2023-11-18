65b4462cbb08a01a419486c72e8dcf9015e90413
krizhnaa <krizhnaprem@gmail.com>
2023-11-18 21:54:50 +0530

    Better Readibilty of the Program

--- test/main.py
+++ test/main.py
@@ -1,16 +1,17 @@
-import sys, fileinput
+# Creating a list called names to store all the names
 names = []
 
+# Reading all the names into the list
 with open("./Input/Names/invited_names.txt") as file:
     names_file = file.read()
     names = names_file.split('\n')
 
-# data_into_list = data.replace('\n', ' ')
-
+# Looping through the names in the list and replacing it with the placeholder in the letter for all the names
 with open("./Input/Letters/starting_letter.txt", mode='r') as file:
     letter = file.read()
     for name in names:
-        replaced_letter = letter.replace('name', name)
+        replaced_letter = letter.replace('[name]', name)
+        # Writing the new letter with replaced name into the folder
         with open(f'./Output/ReadyToSend/{name}_letter.txt', mode='w') as filee:
             filee.write(replaced_letter)
 
