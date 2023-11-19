d6ace43d80251bb860cf0d0d25e9892f40c5831a
krizhnaa <krizhnaprem@gmail.com>
2023-11-19 11:23:27 +0530

    Reading using the CSV reader function

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
-with open('./weather_data.csv') as file:
-    temp = file.readlines()
-    data = []
-    for _ in temp:
-        new_data = _.strip()
-        data.append(new_data)
-    print(data)
+# with open('./weather_data.csv') as file:
+#     temp = file.readlines()
+#     data = []
+#     for _ in temp:
+#         new_data = _.strip()
+#         data.append(new_data)
+#     print(data)
+
+import csv
+
+with open('./weather_data.csv') as data_file:
+    data = csv.reader(data_file)
+    for row in data:
+        print(row)
