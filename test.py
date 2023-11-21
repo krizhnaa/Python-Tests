372ab198c241761b0b2260818a5abc3cbfb84fc7
krizhnaa <krizhnaprem@gmail.com>
2023-11-21 21:46:04 +0530

    Checking for the common numbers in both the text files

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
+with open('file1.txt', 'r') as file1:
+    list1 = [ int(num.rstrip()) for num in file1.readlines()]
+
+with open('file2.txt', 'r') as file2:
+    list2 = [ int(num.rstrip()) for num in file2.readlines()]
 
 
-new_list = [ n*2 for n in range(1, 5)]
-print(new_list)
+result = [ num for num in list1 if num in list2]
+
+
+# # Write your code above 👆
+print(result)
