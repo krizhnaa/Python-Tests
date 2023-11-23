83e979e2f385387b2f19330fe7119f1d4b281c5b
krizhnaa <krizhnaprem@gmail.com>
2023-11-23 20:36:01 +0530

    Completed the coding challenge

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-sentence = input()
+sentence = input("Enter Your Sentence : ").split()
 # 🚨 Don't change code above 👆
 # Write your code below 👇
 
-
+result = { sent: len(sent) for sent in sentence}
 
 
 print(result)
