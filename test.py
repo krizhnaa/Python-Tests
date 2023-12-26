c511a6ff95756db340027cfa0311538ca85c4c1b
krizhnaa <krizhnaprem@gmail.com>
2023-12-26 18:51:11 +0530

    Button and command function

--- test.py
+++ test.py
@@ -1,17 +1,21 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("Gui Window")
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="Bro WTF", font=("Ariel", 24, "bold"))
+my_label = Label(text="Bro WTF", font=("Ariel", 24, "bold"))
 my_label.pack()
 
 my_label["text"] = "Lol I changed it"
 my_label.config(text="Lololol")
 
 
+def button_clicked():
+    my_label["text"] = "CLickeddd"
 
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
 
 
 
