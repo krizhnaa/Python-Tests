93fff3d23572eb92ec0be8a5a6f2dc8eae6227e2
krizhnaa <krizhnaprem@gmail.com>
2023-12-26 19:18:47 +0530

    Done with learning about Labels, buttons and input

--- test.py
+++ test.py
@@ -4,14 +4,8 @@ window = Tk()
 window.title("Gui Window")
 window.minsize(width=500, height=300)
 
-my_label = Label(text="Bro WTF", font=("Ariel", 24, "bold"))
-my_label.pack()
-
-my_label["text"] = "Lol I changed it"
-my_label.config(text="Lololol")
-
-input = Entry()
-input.pack()
+my_label = Label(text="Text thanee", font=("Ariel", 24, "bold"))
+my_label.grid(column=0, row=0)
 
 def button_clicked():
     display_this = input.get()
@@ -19,7 +13,14 @@ def button_clicked():
 
 
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(row=1, column=1)
+
+button = Button(text="Don't", command=button_clicked)
+button.grid(row=0, column=2)
+
+input = Entry()
+input.grid(row=2, column=3)
+
 
 
 
