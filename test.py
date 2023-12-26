d9ae757d62c5e123771e05eaf98404d4618bf012
krizhnaa <krizhnaprem@gmail.com>
2023-12-26 18:44:36 +0530

    Tinkering around with Tkinter

--- test.py
+++ test.py
@@ -1,8 +1,28 @@
+import tkinter
 
-def add(*nums):
-    sum = 0
-    for n in nums:
-        sum+=n
-    print(sum)
+window = tkinter.Tk()
+window.title("Gui Window")
+window.minsize(width=500, height=300)
 
-add(2, 5, 10, 54)
+my_label = tkinter.Label(text="Bro WTF", font=("Ariel", 24, "bold"))
+my_label.pack()
+
+my_label["text"] = "Lol I changed it"
+my_label.config(text="Lololol")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
