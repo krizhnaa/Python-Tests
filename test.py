dd82adece840ad4970cff6c8802b1af204102bfe
krizhnaa <krizhnaprem@gmail.com>
2023-12-29 14:08:23 +0530

    Learning abt Raising an Error

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
 
-try:
-    txt_file = open("file.txt", 'r')
-except FileNotFoundError:
-    txt_file = open("file.txt", 'w')
-    txt_file.write("It has content now")
-    # print("Some error")
-else:
-    content = txt_file.read()
-    print(content)
-finally:
-    print("IDC")
+# try:
+#     txt_file = open("file.txt", 'r')
+# except FileNotFoundError:
+#     txt_file = open("file.txt", 'w')
+#     txt_file.write("It has content now")
+#     # print("Some error")
+# else:
+#     content = txt_file.read()
+#     print(content)
+# finally:
+#     print("IDC")
+
+height = int(input("Enter human Height : "))
+
+if height>3:
+    raise ValueError("Man ur a freaking Godzilla")
+
+print(height)
