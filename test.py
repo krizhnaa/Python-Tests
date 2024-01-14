3dfd85bcdcd500905c9f6ca757dfc363acad9fe8
krizhnaa <krizhnaprem@gmail.com>
2024-01-14 08:34:57 +0530

    Done with Kanye Quotes with API

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 from tkinter import *
+import requests
+
+
 
 
 def get_quote():
-    pass
-    #Write your code here.
+    response = requests.get(url="https://api.kanye.rest/")
+    response.raise_for_status()
+    quotes = response.json()['quote']
+    canvas.itemconfig(quote_text, text=quotes)
 
 
 
