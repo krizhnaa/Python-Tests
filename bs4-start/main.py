2f1c3db0dfc6539fdf0c8eb2f425d9ed74ee0fbf
krizhnaa <krizhnaprem@gmail.com>
2024-01-20 19:33:20 +0530

    Web Scrapping Baby

--- bs4-start/main.py
+++ bs4-start/main.py
@@ -1,17 +1,60 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("website.html", encoding="utf8") as file:
-    contents = file.read()
+response = requests.get("https://news.ycombinator.com/")
+html_file = response.text
 
-soup = BeautifulSoup(contents, "html.parser")
-# print(soup.p)
+soup = BeautifulSoup(html_file, "html.parser")
 
-allanchor = soup.find_all("a")
+anchor_tag = soup.find_all("span", class_="titleline")
+anchor_texts = [ anchor.get_text() for anchor in anchor_tag]
+print(anchor_texts)
 
-# for anchor in allanchor:
-#     print(anchor.get("href"))
+anchor_upvote = [ int(score.get_text().split()[0]) for score in soup.find_all("span", class_="score")]
+max_upvotes = min(anchor_upvote)
+max_index = anchor_upvote.index(min(anchor_upvote))
+print(f"The lowest upvote is {max_upvotes}, for the article '{anchor_texts[max_index]} {max_index}'")
 
-lol = soup.select(selector=".heading")
 
-for _ in lol:
-    print(_.string)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# with open("website.html", encoding="utf8") as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, "html.parser")
+# # print(soup.p)
+#
+# allanchor = soup.find_all("a")
+#
+# # for anchor in allanchor:
+# #     print(anchor.get("href"))
+#
+# lol = soup.select(selector=".heading")
+#
+# for _ in lol:
+#     print(_.string)
+
