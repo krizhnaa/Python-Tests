9e66b863787c1f0c08d211016ebe01d0c723c4b3
krizhnaa <krizhnaprem@gmail.com>
2024-01-24 20:25:29 +0530

    Getting my Github profile name

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option(name="detach", value=True)
 driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
 
-URL = "https://www.flipkart.com/kissan-hazelnut-choco-peanut-spread-350-g/p/itmb2d3625ed1724?pid=JASGG44MPAHSQUUE&lid=LSTJASGG44MPAHSQUUEP22FVR&marketplace=FLIPKART&store=eat%2Fxhv&srno=b_1_2&otracker=browse&fm=organic&iid=en_-FTkwyChchLAImqdr77LZdquq17b6PS3YhGRekqdl6pdEdUEyaPz7GA42ygBVIVsnuu6BWU1uF4zmfluhKnfofUFjCTyOHoHZs-Z5_PS_w0%3D&ppt=browse&ppn=browse&ssid=a1sirsvpkw0000001706106955462"
+URL = "https://github.com/krizhnaa"
 driver.get(url=URL)
 
-price = driver.find_element_by_class_name("_30jeq3._16Jk6d")
-print(price.text)
+name = driver.find_element(By.XPATH, value='/html/body/div[1]/div[4]/main/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/h1/span[1]')
+print(name.text)
 
 driver.quit()
