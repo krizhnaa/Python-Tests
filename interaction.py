3f516f6236faf820911477f24d95f40d8d9f3c00
krizhnaa <krizhnaprem@gmail.com>
2024-01-25 09:33:27 +0530

    Automating Filling of forms by using selenium

--- interaction.py
+++ interaction.py
@@ -1,16 +1,26 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path = ChromeDriverManager().install()
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option(name="detach", value=True)
 driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
 
-URL = "https://en.wikipedia.org/wiki/Main_Page"
+URL = "https://secure-retreat-92358.herokuapp.com/"
 driver.get(url=URL)
 
-no_of_articles = driver.find_element(By.XPATH, value='//*[@id="articlecount"]/a')
-print(no_of_articles.text)
+f_name = driver.find_element(By.NAME, value="fName")
+f_name.send_keys("Krizhna")
 
-driver.quit()
+l_name = driver.find_element(By.NAME, value="lName")
+l_name.send_keys("Brem")
+
+mail = driver.find_element(By.NAME, value="email")
+mail.send_keys("lololol@gmail.com")
+
+butt = driver.find_element(By.CLASS_NAME, value="btn")
+butt.click()
+
+# driver.quit()
