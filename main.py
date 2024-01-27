6bacc9e98c17681a6cae886819234749eaeb54c8
krizhnaa <krizhnaprem@gmail.com>
2024-01-27 09:09:30 +0530

    Decorator Functions

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-from flask import Flask
+# from flask import Flask
+#
+# app = Flask(__name__)
+# print(__name__)
+#
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+#
+# if __name__ == "__main__":
+#     app.run()
 
-app = Flask(__name__)
+import time
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+def decorator_func(function):
+    def wrapper_function():
+        time.sleep(1)
+        function()
+        time.sleep(1)
+        function()
+    return wrapper_function
+
+
+@decorator_func
+def say_krish():
+    print("Krishhh")
+
+
+say_krish()
