76b58d9bd7b73b2c7b6ed6f8bbd6bfd644f17f15
krizhnaa <krizhnaprem@gmail.com>
2024-01-29 20:43:39 +0530

    Made a sample API responsive flask program

--- server.py
+++ server.py
@@ -6,11 +6,18 @@ app = Flask(__name__)
 @app.route("/guess/<name>")
 def home(name):
     gender_response = requests.get(url=f"https://api.genderize.io?name={name}")
-    gr = gender_response.json()
+    gr = gender_response.json()['gender']
     age_response = requests.get(url=f"https://api.agify.io?name={name}")
-    ar = age_response.json()
-    return render_template('index.html', gr=gr, ar=ar)
+    ar = age_response.json()['age']
+    return render_template('index.html', name=name, gr=gr, ar=ar)
 
+# name = "Angela"
+# gender_response = requests.get(url=f"https://api.genderize.io?name={name}")
+# gr = gender_response.json()['gender']
+# age_response = requests.get(url=f"https://api.agify.io?name={name}")
+# ar = age_response.json()['age']
+# print(gr)
+# print(ar)
 
 if __name__ == "__main__":
     app.run(debug=True)
