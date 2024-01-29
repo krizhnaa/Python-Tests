e0b08545b14445d454749424eb597e57dfcc1eb8
krizhnaa <krizhnaprem@gmail.com>
2024-01-29 17:54:54 +0530

    Rendering the current year for the footer

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 import random
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     random_num = random.randint(0, 10)
-    return render_template('index.html', random_num=random_num)
+    current_year = datetime.now().year
+    return render_template('index.html', random_num=random_num, year=current_year)
 
 
 if __name__ == "__main__":
